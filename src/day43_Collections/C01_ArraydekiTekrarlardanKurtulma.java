a8f51b18ff053ee136e1cba47a69463be0411e84
mstfkarakas <mstfkarakas@gmail.com>
2023-04-13 08:29:38 +1000

    ...

--- src/day43_Collections/C01_ArraydekiTekrarlardanKurtulma.java
+++ src/day43_Collections/C01_ArraydekiTekrarlardanKurtulma.java
@@ -7,7 +7,7 @@ import java.util.Set;
 public class C01_ArraydekiTekrarlardanKurtulma {
     public static void main(String[] args) {
 
-                 /*
+        /*
         Soru:
         Verilen bir arraydeki tekrar eden elementleri sadece 1 kere yazdıracak array'i kısaltan bir kod yazınız
         Hint: SET Kullanınız.
@@ -20,12 +20,11 @@ public class C01_ArraydekiTekrarlardanKurtulma {
         for (int each:arr
         ) {
             benzersizSet.add(each);
-
         }
         System.out.println(benzersizSet);//[1, 2, 3, 4, 5, 6, 7]
         System.out.println(Arrays.toString(arr));//[1,2,1,3,4,5,1,2,6,2,3,4,5,1,3,2,6,5,7]
 
-        arr=new int[benzersizSet.size()]; // [0,0,0,0,0,0,0]
+        arr = new int [benzersizSet.size()]; // [0,0,0,0,0,0,0]
 
         int index=0;
         for (int each:benzersizSet
@@ -34,7 +33,7 @@ public class C01_ArraydekiTekrarlardanKurtulma {
             index++;
 
         }
-        System.out.println("array'in son hali: "+ Arrays.toString(arr));
+        System.out.println("array'in son hali: "+ Arrays.toString(arr)); // [1, 2, 3, 4, 5, 6, 7]
 
     }
 }
